b449dfa54d120d4f8bb8da5d16b8e98a58d12b3a
DerekY2 <derek.yu869@gmail.com>
2025-03-14 23:06:06 -0400

    lambda functions

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', function() {
+const button = document.getElementById('myButton');
+    button.addEventListener('click', ()=> {
         // Handle button click
         alert('Button clicked!');
-    });
 });
